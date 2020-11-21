6709536ea27a46bf2bf3232340cc5da07b3382c3
lequocduyquang <quang.le@vexere.com>
2020-11-21 16:06:45 +0700

    update user dto

--- domain/users/users_dto.go
+++ domain/users/users_dto.go
@@ -3,34 +3,82 @@ package users
 import (
 	"errors"
 	"strings"
+
+	"github.com/badoux/checkmail"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User define model
 type User struct {
-	ID       int64  `json:"id"`
-	UserName string `json:"username"`
-	Email    string `json:"email"`
+	gorm.Model
+	Email    string `gorm:"type:varchar(100);unique_index" json:"email"`
+	UserName string `gorm:"size:100;not null" json:"username"`
+	Password string `gorm:"size:100;not null" json:"password"`
+	Avatar   string `gorm:"size:255" json:"avatar"`
 	Status   bool   `json:"status"`
-	Password string `json:"password"`
 }
 
 // Users define model
 type Users []User
 
-// Validate function
-func (u *User) Validate() error {
-	u.UserName = strings.TrimSpace(u.UserName)
-	u.Email = strings.TrimSpace(u.Email)
-	u.Password = strings.TrimSpace(u.Password)
+// HashPassword hashes password from user input
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
 
-	if u.UserName == "" {
-		return errors.New("Invalid username")
-	}
-	if u.Email == "" {
-		return errors.New("Invalid email")
-	}
-	if u.Password == "" {
-		return errors.New("Invalid password")
+// CheckPasswordHash checks password hash and password from user input
+func CheckPasswordHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return errors.New("Incorrect password")
 	}
 	return nil
+}
+
+// BeforeSave hashes user password
+func (u *User) BeforeSave() error {
+	password := strings.TrimSpace(u.Password)
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+// Prepare strips user input of any white spaces
+func (u *User) Prepare() {
+	u.UserName = strings.TrimSpace(u.UserName)
+	u.Email = strings.TrimSpace(u.Email)
+	u.Avatar = strings.TrimSpace(u.Avatar)
+}
+
+// Validate function
+func (u *User) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "login":
+		if u.Email == "" {
+			return errors.New("Email is required")
+		}
+		if u.Password == "" {
+			return errors.New("Password is required")
+		}
+		return nil
+	default:
+		if u.UserName == "" {
+			return errors.New("Username is required")
+		}
+		if u.Email == "" {
+			return errors.New("Email is required")
+		}
+		if u.Password == "" {
+			return errors.New("Password is required")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("Invalid Email")
+		}
+		return nil
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/lequocduyquang/user-profile-service
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/badoux/checkmail v1.2.1 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
+)
